6eb410bff08283c4a007a8e7f18baff0d32aa95d
xfeaton <xfeaton@gmail.com>
2014-11-28 03:38:30 +0200

    add job routing

--- src/App/JoboardBundle/Entity/Job.php
+++ src/App/JoboardBundle/Entity/Job.php
@@ -4,6 +4,9 @@ namespace App\JoboardBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use App\JoboardBundle\Utils\Joboard as Joborad;
+
+
 /**
  * Job
  */
@@ -490,4 +493,20 @@ class Job
     {
         $this->updated_at = new \DateTime();
     }
+
+    public function getCompanySlug()
+    {
+        return Joborad::slugify($this->getCompany());
+    }
+
+    public function getPositionSlug()
+    {
+        return Joborad::slugify($this->getPosition());
+    }
+
+    public function getLocationSlug()
+    {
+        return Joborad::slugify($this->getLocation());
+    }
+
 }
